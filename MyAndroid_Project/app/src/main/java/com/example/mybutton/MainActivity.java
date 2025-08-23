0d7b0282b6383e13766f4ad6501f1bcc0f52a3a2
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-08-24 00:42:39 +0600

    Button with Listener part - 1

--- MyAndroid_Project/app/src/main/java/com/example/mybutton/MainActivity.java
+++ MyAndroid_Project/app/src/main/java/com/example/mybutton/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mybutton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    int count = 0;
+    private Button loginButton;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        loginButton = (Button) findViewById(R.id.loginButtonId);
+        textView = (TextView) findViewById(R.id.textViewId);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                textView.setText("Login Button is clicked "+count +" times");
+            }
         });
+
     }
 }
