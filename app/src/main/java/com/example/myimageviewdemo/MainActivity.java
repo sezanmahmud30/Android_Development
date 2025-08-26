8f7ea550367b6a4227a543df8e0f333194cf95dc
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-08-27 03:53:22 +0600

    Displaying Image part 1 & 2

---
+++ app/src/main/java/com/example/myimageviewdemo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myimageviewdemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imageView1,imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        imageView1 = findViewById(R.id.imageView1Id);
+        imageView2 = findViewById(R.id.imageView2Id);
+
+
+        imageView1.setOnClickListener(this);
+        imageView2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId()==R.id.imageView2Id){
+            Toast.makeText(MainActivity.this,"Sezan Mahmud 2024",Toast.LENGTH_SHORT).show();
+
+        }
+        else if(v.getId()==R.id.imageView1Id){
+            Toast.makeText(MainActivity.this,"Sezan Mahmud 2025",Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+}
