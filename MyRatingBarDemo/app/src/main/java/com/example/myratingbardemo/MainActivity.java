2ee9725f7591f99e69027359a0a1580477f29339
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-09-01 01:09:25 +0600

    Ratingbar

---
+++ MyRatingBarDemo/app/src/main/java/com/example/myratingbardemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myratingbardemo;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = (RatingBar) findViewById(R.id.ratingBarId);
+        textView = (TextView) findViewById(R.id.textViewId);
+
+
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+
+                textView.setText("Value : "+rating);
+            }
+        });
+
+    }
+}
