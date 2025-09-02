8f9d5a32500cd5eac11765e346b4b814e92ccb4f
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-09-03 02:46:34 +0600

    Tejgaon College Website Convert Android Application

---
+++ TejgaonCollege/app/src/main/java/com/example/tejgaoncollege/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.tejgaoncollege;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webViewId);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://www.tejgaoncollege.edu.bd/");
+
+    }
+}
