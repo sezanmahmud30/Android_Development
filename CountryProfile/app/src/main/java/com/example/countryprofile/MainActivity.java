e0344476b9ca6517ac0884e794ef27bf12f8b48c
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-09-11 02:18:04 +0600

    Country Profile Project part - 2

--- CountryProfile/app/src/main/java/com/example/countryprofile/MainActivity.java
+++ CountryProfile/app/src/main/java/com/example/countryprofile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.countryprofile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button bangladeshButton,indiaButton,pakistanButton;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        bangladeshButton = (Button) findViewById(R.id.bangladeshButtonId);
+        indiaButton = (Button) findViewById(R.id.indiaButtonId);
+        pakistanButton = (Button) findViewById(R.id.pakistanButtonId);
+
+        bangladeshButton.setOnClickListener(this);
+        indiaButton.setOnClickListener(this);
+        pakistanButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.bangladeshButtonId){
+
+            intent = new Intent(MainActivity.this,ProfileActivity.class);
+            intent.putExtra("name","bangladesh");
+            startActivity(intent);
+        }
+
+        if(v.getId() == R.id.indiaButtonId){
+
+            intent = new Intent(MainActivity.this,ProfileActivity.class);
+            intent.putExtra("name","india");
+            startActivity(intent);
+        }
+
+        if(v.getId() == R.id.pakistanButtonId){
+
+            intent = new Intent(MainActivity.this,ProfileActivity.class);
+            intent.putExtra("name","pakistan");
+            startActivity(intent);
+        }
+
     }
 }
--- CountryProfile/app/src/main/java/com/example/countryprofile/ProfileActivity.java
+++ CountryProfile/app/src/main/java/com/example/countryprofile/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.countryprofile;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,46 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        imageView = (ImageView) findViewById(R.id.imageViewID);
+        textView = (TextView) findViewById(R.id.textViewId);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+
+            String countryName = bundle.getString("name");
+
+
+            showDetails(countryName);
+        }
+
+    }
+
+    void showDetails(String countryName){
+
+        if(countryName.equals("bangladesh")){
+            imageView.setImageResource(R.drawable.bangladesh_national);
+            textView.setText(R.string.bangladesh_text);
+        }
+
+        if(countryName.equals("india")){
+            imageView.setImageResource(R.drawable.indian_parliament);
+            textView.setText(R.string.india_text);
+        }
+
+        if(countryName.equals("pakistan")){
+            imageView.setImageResource(R.drawable.pakistan_parliament);
+            textView.setText(R.string.pakistan_text);
+        }
+
     }
 }
