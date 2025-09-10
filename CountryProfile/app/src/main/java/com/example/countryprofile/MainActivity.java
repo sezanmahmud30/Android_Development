f88ed7321a4d244a812f03b911d1213cc423facf
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-09-11 03:54:22 +0600

    Country Profile Project part - 3

--- CountryProfile/app/src/main/java/com/example/countryprofile/MainActivity.java
+++ CountryProfile/app/src/main/java/com/example/countryprofile/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.countryprofile;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -56,6 +58,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.putExtra("name","pakistan");
             startActivity(intent);
         }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        AlertDialog.Builder alertDialogBuilder;
+        alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+
+        alertDialogBuilder.setIcon(R.drawable.question);
+        alertDialogBuilder.setTitle(R.string.title_text);
+        alertDialogBuilder.setMessage(R.string.message_text);
+        alertDialogBuilder.setCancelable(false);
+
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                finish();
+            }
+        });
+
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
 
     }
 }
