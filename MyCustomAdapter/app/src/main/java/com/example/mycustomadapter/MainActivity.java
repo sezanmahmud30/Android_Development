092c2dcea851978a4542880e2d0b8cf695f04f0a
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-09-13 05:53:03 +0600

    Listview Custom Adapter Android part-  1

---
+++ MyCustomAdapter/app/src/main/java/com/example/mycustomadapter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mycustomadapter;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listViewId);
+
+
+
+    }
+}
