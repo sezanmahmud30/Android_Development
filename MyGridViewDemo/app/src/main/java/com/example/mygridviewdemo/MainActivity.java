978e6e8f430db1cb6cdfda7f3984ed9c2b47ae86
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-09-19 17:12:18 +0600

    Gridview Android (Part-2) add Toast

--- MyGridViewDemo/app/src/main/java/com/example/mygridviewdemo/MainActivity.java
+++ MyGridViewDemo/app/src/main/java/com/example/mygridviewdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mygridviewdemo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
 
         CustomAdapter adapter = new CustomAdapter(this,countryNames,flags);
         gridView.setAdapter(adapter);
+
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String value = countryNames[position];
+                Toast.makeText(MainActivity.this,value,Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
