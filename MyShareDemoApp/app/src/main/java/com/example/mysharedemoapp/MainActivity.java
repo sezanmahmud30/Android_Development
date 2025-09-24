b28e31c17f96e64e8df88061cdb301fc668d6786
Sezan Mahmud <sezanmahmud789@gmail.com>
2025-09-25 01:35:03 +0600

    Adding feedback menu item in action bar (part-1)

---
+++ MyShareDemoApp/app/src/main/java/com/example/mysharedemoapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mysharedemoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_item,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()== R.id.shareId){
+
+
+        }
+        else if(item.getItemId()==R.id.feedbackId){
+
+            Intent intent = new Intent(getApplicationContext(),FeedbackActivity.class);
+            startActivity(intent);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
